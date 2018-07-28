f71644e67c98d021772f1aba61f6659212c886c4
DeccaDc <lzwneu@163.com>
2018-07-28 09:01:16 +0800

    1

--- src/main/java/neu/lab/conflict/DebugMojo.java
+++ src/main/java/neu/lab/conflict/DebugMojo.java
@@ -38,8 +38,8 @@ public class DebugMojo extends ConflictMojo {
 		// "classDupByJar.txt");
 
 		// TODO what's debug?
-		// printMthdProb();
-		printSize();
+		 printMthdProb();
+//		printSize();
 	}
 
 	private void printSize() {
